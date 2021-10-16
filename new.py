697c8c17e6207484d876fda941dae319ab217486
Thomas Marwitz <thomasmarwitz3@gmail.com>
2021-10-16 14:30:32 +0200

    mailer obj with test message method

--- new.py
+++ new.py
@@ -8,12 +8,7 @@ import pandas as pd
 
 load_dotenv()
 
-try:
-    email:      str = os.environ["EMAIL"]
-    password:   str = os.environ["PASSWORD"]
-except KeyError:
-    print("Please specify EMAIL and PASSWORD in the .env file")
-    raise SystemExit
+# alvindeguzman@hotmail.de
 
 class Message:
 
@@ -35,11 +30,52 @@ class Manager:
         for (name1, email1, name2, email2) in zip(self.df["Name1"], self.df["Email1"], self.df["Name2"], self.df["Email2"]):
             pass
 
-manager: Manager = Manager("Zuordnung.xlsx")
-
 
 # email stuff
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from email.header import Header
 from email.utils import formataddr
 
-formataddr((str(Header('Someone Somewhere', 'utf-8')), 'xxxxx@gmail.com'))
+class Mailer:
+
+    def __init__(self):
+        try:
+            self.email:             str = os.environ["EMAIL"]
+            self.password:          str = os.environ["PASSWORD"]
+            self.host_address:      str = os.environ["HOST_ADDRESS"]
+            self.port:              str = os.environ["PORT"]
+        except KeyError:
+            print("Please specify EMAIL and PASSWORD in the .env file")
+            raise SystemExit
+
+        self.server = smtplib.SMTP(host=self.host_address, port=self.port)
+        self.server.set_debuglevel(0)
+        self.server.starttls()
+        self.server.login(self.email, self.password)
+
+        
+    
+    def send_test_message(self):
+        msg = MIMEMultipart()
+        
+        msg['From'] =   formataddr((str(Header('Von Sender', 'utf-8')), self.email))
+        msg['To'] =     formataddr((str(Header('An Empfaenger', "utf-8")), self.email))
+        msg['Subject'] = "Automated Test E-Mail"
+        
+        msg.attach(MIMEText(
+            Message("message.html").replaceValues({}),
+            "html")
+        )
+
+        self.server.send_message(msg)
+
+
+
+        
+
+
+manager: Manager = Manager("Zuordnung.xlsx")
+
+
+
