2242cde79dfc8bbdc7f49c6ac421df47ceadde45
Thomas Marwitz <thomasmarwitz3@gmail.com>
2021-10-16 13:59:36 +0200

    message obj

--- new.py
+++ new.py
@@ -14,5 +14,13 @@ except KeyError:
     print("Please specify EMAIL and PASSWORD in the .env file")
     raise SystemExit
 
+class Message:
 
-print(email, password)
+    def __init__(self, file_name: str) -> None:
+        self.txt: str= open(file_name).read()
+
+    def replaceValues(self, data: dict[str, str]) -> str:
+        text: str = self.txt # don't modify original txt
+        for key, value in data.items():
+            text = text.replace("{{" + key + "}}", value)
+        return text
